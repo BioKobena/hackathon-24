2cd0aecbd0cc844b930d0c60ce747a1403c1432a
Bio Paul Kobena <biokobena07@gmail.com>
2024-03-03 11:28:21 +0000

    finish

--- result/src/containers/Analytics/ListStudent.jsx
+++ result/src/containers/Analytics/ListStudent.jsx
@@ -86,10 +86,10 @@ export default function ListStudent() {
                             <TableHead>
                                 <TableRow>
                                     <TableCell align="left" sx={{ fontWeight: "bold", backgroundColor: "#FFAFAA", color: '#FFFFFF', fontFamily: "Montserrat", padding: 3, borderBlock:1,textAlign:"center", fontSize:"1.5rem"}}> <span style={{color:"green"}}>20005</span> <br/> INSCRITS </TableCell>
-                                    <TableCell align="left" sx={{ fontWeight: "bold", backgroundColor: "#FFAFAA", color: '#FFFFFF', fontFamily: "Montserrat", padding: 3, borderBlock:1,textAlign:"center", fontSize:"1.5rem" }}> <span style={{color:"green"}}>20005</span> <br/>TOTAL VOTANT</TableCell>
-                                    <TableCell align="left" sx={{ fontWeight: "bold", backgroundColor: "#FFAFAA", color: '#FFFFFF', fontFamily: "Montserrat", padding: 3, borderBlock:1,textAlign:"center", fontSize:"1.5rem" }}> <span style={{color:"green"}}>20005</span> <br/>TAUX DE PART.T</TableCell>
-                                    <TableCell align="left" sx={{ fontWeight: "bold", backgroundColor: "#FFAFAA", color: '#FFFFFF', fontFamily: "Montserrat", padding: 3, borderBlock:1,textAlign:"center", fontSize:"1.5rem" }}> <span style={{color:"green"}}>20005</span> <br/>BULLETINS NULS</TableCell>
-                                    <TableCell align="left" sx={{ fontWeight: "bold", backgroundColor: "#FFAFAA", color: '#FFFFFF', fontFamily: "Montserrat", padding: 3, borderBlock:1,textAlign:"center", fontSize:"1.5rem" }}> <span style={{color:"green"}}>20005</span> <br/>SUFFRAGE EXPRIMÉ</TableCell>
+                                    <TableCell align="left" sx={{ fontWeight: "bold", backgroundColor: "#FFAFAA", color: '#FFFFFF', fontFamily: "Montserrat", padding: 3, borderBlock:1,textAlign:"center", fontSize:"1.5rem" }}> <span style={{color:"green"}}>17005</span> <br/>TOTAL VOTANT</TableCell>
+                                    <TableCell align="left" sx={{ fontWeight: "bold", backgroundColor: "#FFAFAA", color: '#FFFFFF', fontFamily: "Montserrat", padding: 3, borderBlock:1,textAlign:"center", fontSize:"1.5rem" }}> <span style={{color:"green"}}>89%</span> <br/>TAUX DE PARTICIPATION</TableCell>
+                                    <TableCell align="left" sx={{ fontWeight: "bold", backgroundColor: "#FFAFAA", color: '#FFFFFF', fontFamily: "Montserrat", padding: 3, borderBlock:1,textAlign:"center", fontSize:"1.5rem" }}> <span style={{color:"green"}}>205</span> <br/>BULLETINS NULS</TableCell>
+                                    <TableCell align="left" sx={{ fontWeight: "bold", backgroundColor: "#FFAFAA", color: '#FFFFFF', fontFamily: "Montserrat", padding: 3, borderBlock:1,textAlign:"center", fontSize:"1.5rem" }}> <span style={{color:"green"}}>15005</span> <br/>SUFFRAGE EXPRIMÉ</TableCell>
                                 </TableRow>
                             </TableHead>
                         </Table>
@@ -97,7 +97,7 @@ export default function ListStudent() {
                 </Paper>
 
                 <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", marginBottom:5}}>
-                    <Paper sx={{width:'22%', height:"80%",position: "absolute", top: 365, left:75}} elevation={2}>
+                    <Paper sx={{width:'22%', height:"80%",position: "absolute", top: 380, left:75}} elevation={2}>
                     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: 5 }}>
                 <Avatar src="https://media.licdn.com/dms/image/D4E03AQGa9sTQUC8tRQ/profile-displayphoto-shrink_800_800/0/1697301555895?e=1714608000&v=beta&t=_DEUTCJy5OuLH_t9_uRLim9z19ao22kb1FMfxUqakSU" alt="Bio Paul" sx={{ width: 320, height: 500, borderRadius:3, background:"#000", marginTop:-1 }} />
                 <Typography variant="h6" sx={{ textAlign: "center", fontFamily: "Montserrat", fontWeight: "800", fontSize:"2rem", color:"#FF7200" }}>
@@ -109,7 +109,7 @@ export default function ListStudent() {
                  </Alert>
                 </div>
                     </Paper>
-                    <Paper sx={{width:'67%', height:"80%",position: "absolute", top: 365, right:75}} elevation={2}>
+                    <Paper sx={{width:'67%', height:"80%",position: "absolute", top: 380, right:75}} elevation={2}>
                         <div style={{width:'100%'}}>
                         <Grid style={{ display: "flex", justifyContent: "center", alignItems: "center", flexWrap: "wrap", marginTop: 100, width:"100%" }}>
                         <Chart options={chartData.options} series={chartData.series} type="bar" width={500} height={320} />
@@ -121,6 +121,45 @@ export default function ListStudent() {
 
                 </div>
             </Box>
+            <TableContainer sx={{ mt: 2, borderRadius: 1, position:"absolute", top:1050 }}>
+    <Table>
+        <TableHead>
+            <TableRow>
+                <TableCell align="left" sx={{ fontWeight: "bold", backgroundColor: "#FFAFAA", color: '#FFFFFF', fontFamily: "Montserrat", padding: 3, borderBlock: 1, textAlign: "center" }}>ID</TableCell>
+                <TableCell align="left" sx={{ fontWeight: "bold", backgroundColor: "#FFAFAA", color: '#FFFFFF', fontFamily: "Montserrat", padding: 3, borderBlock: 1, textAlign: "center" }}>Numéro de Vote</TableCell>
+                <TableCell align="left" sx={{ fontWeight: "bold", backgroundColor: "#FFAFAA", color: '#FFFFFF', fontFamily: "Montserrat", padding: 3, borderBlock: 1, textAlign: "center" }}>Electeurs</TableCell>
+            </TableRow>
+        </TableHead>
+        <TableBody>
+    <TableRow>
+        <TableCell align="center">1</TableCell>
+        <TableCell align="center">VOTE-001</TableCell>
+        <TableCell align="center">Electeur 1</TableCell>
+    </TableRow>
+    <TableRow>
+        <TableCell align="center">2</TableCell>
+        <TableCell align="center">VOTE-002</TableCell>
+        <TableCell align="center">Electeur 2</TableCell>
+    </TableRow>
+    <TableRow>
+        <TableCell align="center">3</TableCell>
+        <TableCell align="center">VOTE-003</TableCell>
+        <TableCell align="center">Electeur 3</TableCell>
+    </TableRow>
+    <TableRow>
+        <TableCell align="center">4</TableCell>
+        <TableCell align="center">VOTE-004</TableCell>
+        <TableCell align="center">Electeur 4</TableCell>
+    </TableRow>
+    <TableRow>
+        <TableCell align="center">5</TableCell>
+        <TableCell align="center">VOTE-005</TableCell>
+        <TableCell align="center">Electeur 5</TableCell>
+    </TableRow>
+</TableBody>
+    </Table>
+</TableContainer>
+
         </Box>
     )
 }
